a2f2c6215d4a7fe91ae9510e8428088f6367e04b
Marcelo Bomfim <bomfim.m@icloud.com>
2026-04-10 21:54:21 -0300

    Adiciona menu principal e classe Produto

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("-> Sistema de Gestão Comercial Integrado <-\n" +
+                "1 - Cadastrar produto\n" +
+                "2 - Listar produtos\n" +
+                "3 - Atualizar quantidade\n" +
+                "4 - Remover produto\n" +
+                "5 - Sair\n");
+    }
+}
---
+++ src/Produto.java
@@ -0,0 +1,27 @@
+public class Produto {
+    private String nome;
+    private String descricao;
+    private int quantidade;
+
+    public Produto(String nome, String descricao, int quantidade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
