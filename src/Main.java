3834c54410ab264f01483508c18ffcf29eef427f
Marcelo Bomfim <bomfim.m@icloud.com>
2026-04-29 16:20:41 -0300

    finaliza sistema

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,115 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<Produto> produtos = new ArrayList<Produto>();
-        int opcao;
-        while (true) {
+        ArrayList produtos = new ArrayList();
 
-            System.out.print("-> Sistema de Gestão Comercial Integrado <-\n" +
-                    "1 - Cadastrar produto\n" +
-                    "2 - Listar produtos\n" +
-                    "3 - Atualizar quantidade\n" +
-                    "4 - Remover produto\n" +
-                    "5 - Sair\n" +
-                    "> ");
+        int opcao;
+
+        do {
+            System.out.println("\n========================================");
+            System.out.println("   SISTEMA DE LISTA DE COMPRAS");
+            System.out.println("========================================");
+            System.out.println("1 - Inserir item");
+            System.out.println("2 - Remover item pelo nome");
+            System.out.println("3 - Consultar item pelo nome");
+            System.out.println("4 - Exibir todos os itens");
+            System.out.println("5 - Sair");
+            System.out.print("Escolha uma opção: ");
+
             opcao = input.nextInt();
             input.nextLine();
 
             switch (opcao) {
                 case 1:
-
                     System.out.print("Nome do produto: ");
                     String nome = input.nextLine();
-                    System.out.print("Descriço do produto: ");
-                    String descricao = input.nextLine();
+
                     System.out.print("Quantidade do produto: ");
                     int quantidade = input.nextInt();
                     input.nextLine();
 
-                    produtos.add(new Produto(nome, descricao, quantidade));
+                    Produto novoProduto = new Produto(nome, quantidade);
+                    produtos.add(novoProduto);
 
+                    System.out.println("Produto cadastrado com sucesso.");
                     break;
 
                 case 2:
-                    System.out.print("========================================\n" +
-                            "          LISTA DE PRODUTOS\n" +
-                            "========================================\n");
+                    System.out.print("Digite o nome do produto que deseja remover: ");
+                    String nomeRemover = input.nextLine();
 
-                    for (Produto produto : produtos) {
-                        System.out.println("Nome: " + produto.getNome() +
-                                           "\nDescricao: " + produto.getDescricao() +
-                                           "\nQuantidade: " + produto.getQuantidade() +
-                        "-------------------------------------------------------------------------");
+                    boolean removido = false;
+
+                    for (int i = 0; i < produtos.size(); i++) {
+                        Produto produto = (Produto) produtos.get(i);
+
+                        if (produto.getNome().equalsIgnoreCase(nomeRemover)) {
+                            produtos.remove(i);
+                            removido = true;
+                            System.out.println("Produto removido com sucesso.");
+                            break;
+                        }
                     }
 
+                    if (!removido) {
+                        System.out.println("Produto não encontrado.");
+                    }
                     break;
-
 
                 case 3:
+                    System.out.print("Digite o nome do produto que deseja consultar: ");
+                    String nomeConsulta = input.nextLine();
+
+                    boolean encontrado = false;
+
+                    for (int i = 0; i < produtos.size(); i++) {
+                        Produto produto = (Produto) produtos.get(i);
+
+                        if (produto.getNome().equalsIgnoreCase(nomeConsulta)) {
+                            System.out.println("\nProduto encontrado:");
+                            System.out.println("Nome: " + produto.getNome());
+                            System.out.println("Quantidade: " + produto.getQuantidade());
+                            encontrado = true;
+                            break;
+                        }
+                    }
+
+                    if (!encontrado) {
+                        System.out.println("Produto não encontrado.");
+                    }
+                    break;
 
                 case 4:
+                    System.out.println("\n========================================");
+                    System.out.println("         LISTA DE PRODUTOS");
+                    System.out.println("========================================");
+
+                    if (produtos.isEmpty()) {
+                        System.out.println("Nenhum produto cadastrado.");
+                    } else {
+                        for (int i = 0; i < produtos.size(); i++) {
+                            Produto produto = (Produto) produtos.get(i);
+
+                            System.out.println((i + 1) + ". Nome: " + produto.getNome());
+                            System.out.println("   Quantidade: " + produto.getQuantidade());
+                            System.out.println("----------------------------------------");
+                        }
+                    }
+                    break;
 
                 case 5:
-                    System.out.print("Saindo...");
+                    System.out.println("Encerrando o programa...");
                     break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
             }
-        }
+
+        } while (opcao != 5);
+
+        input.close();
     }
 }
--- src/Produto.java
+++ src/Produto.java
@@ -1,20 +1,14 @@
 public class Produto {
     private String nome;
-    private String descricao;
     private int quantidade;
 
-    public Produto(String nome, String descricao, int quantidade) {
+    public Produto(String nome, int quantidade) {
         this.nome = nome;
-        this.descricao = descricao;
         this.quantidade = quantidade;
     }
 
     public String getNome() {
         return nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
     }
 
     public int getQuantidade() {
